347d778a3e7fcdd5fc0890079b0799e4d58bbab4
Dima Ludov <lDummyl>
2021-01-19 18:55:44 +0300

    add 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
-
+        System.out.println(3);
     }
 
     public static void toInject() {
